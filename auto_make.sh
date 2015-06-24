15493a0e37dd02887989f1495ea33deed022a79a
Jay <kwangaho@gmail.com>
2015-06-24 11:26:12 +0800

    5.6.23

--- auto_make.sh
+++ auto_make.sh
@@ -4,7 +4,8 @@
 echo -e "\e[40;32;1m"
 clear
 BUILD_NUMBER=$1
-LAST_TARGET=$2
+LAST_BUILD_NUMBER=$2
+LAST_TARGET="$2"-target.zip
 
 if [ "$1" == "" ];then
 	echo "             Usage: $0 BUILD_NUMBER  LAST_TARGET                     " 
@@ -49,6 +50,8 @@ clean(){
 	fi
 }
 
+GET_FULLOTA_MD5SUM=`md5sum out/fullota.zip |cut -c 1-10`
+
 fullota(){
 	echo "============================"
 	echo "          Fullota           "
@@ -57,8 +60,10 @@ fullota(){
 	echo "============================"
 	echo ">>>  Copying Fullota.zip    "
 	echo "============================"
-	cp out/fullota.zip $BUILD_NUMBER-fullota.zip
+	cp out/fullota.zip miui6_KLTE_luo2888_"$BUILD_NUMBER"_"$GET_FULLOTA_MD5SUM"_4.4.zip
 }
+
+GET_DIFFOTA_MD5SUM=`md5sum diffota.zip |cut -c 1-10`
 
 otadiff(){
 	echo "============================"
@@ -69,7 +74,8 @@ otadiff(){
 	echo "----------------------------"
 	read m
 	if [ "$m" == "y" ]; then
-		../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $LAST_TARGET out/target_files.zip OTA-$LAST_TARGET-$BUILD_NUMBER.zip
+		../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $LAST_TARGET out/target_files.zip out/diffota.zip
+		cp out/diffota.zip miui_ota_KLTE_luo2888_"$LAST_BUILD_NUMBER"_"$BUILD_NUMBER"_"$GET_DIFFOTA_MD5SUM"_4.4.zip
 		echo "================================="
 		echo ">>>  Copying  Target_files.zip   "
 		echo "================================="
--- releasetools.py
+++ releasetools.py
@@ -75,135 +75,8 @@ package_extract_file("kltechn.img", "/tmp/kltechn.img");
 package_extract_file("kltekdi.img", "/tmp/kltekdi.img");
 package_extract_file("kltespr.img", "/tmp/kltespr.img");
 set_metadata("/system/flash_boot", "uid", 0, "gid", 0, "mode", 0755);
-run_program("/system/flash_boot");
-delete("/system/flash_boot");"""
+run_program("/system/flash_boot");"""
     info.script.AppendExtra(extra_img_flash);
-
-def UpdateFirmWare2(info):
-  edify = info.script
-  for i in xrange(len(edify.script)):
-    if "ui_print" in edify.script[i] and "Boot" in edify.script[i]:
-      edify.script[i] = """ui_print("Update FirmWare...");
-symlink("/data/misc/audio/mbhc.bin", "/system/etc/firmware/wcd9320/wcd9320_mbhc.bin");
-symlink("/data/misc/audio/wcd9320_anc.bin", "/system/etc/firmware/wcd9320/wcd9320_anc.bin");
-symlink("/data/misc/audio/wcd9320_mad_audio.bin", "/system/etc/firmware/wcd9320/wcd9320_mad_audio.bin");
-symlink("/firmware/image/keymaste.b00", "/system/vendor/firmware/keymaster/keymaster.b00");
-symlink("/firmware/image/keymaste.b01", "/system/vendor/firmware/keymaster/keymaster.b01");
-symlink("/firmware/image/keymaste.b02", "/system/vendor/firmware/keymaster/keymaster.b02");
-symlink("/firmware/image/keymaste.b03", "/system/vendor/firmware/keymaster/keymaster.b03");
-symlink("/firmware/image/keymaste.mdt", "/system/vendor/firmware/keymaster/keymaster.mdt");
-symlink("/firmware-modem/image/adsp.b00", "/system/etc/firmware/adsp.b00");
-symlink("/firmware-modem/image/adsp.b01", "/system/etc/firmware/adsp.b01");
-symlink("/firmware-modem/image/adsp.b02", "/system/etc/firmware/adsp.b02");
-symlink("/firmware-modem/image/adsp.b03", "/system/etc/firmware/adsp.b03");
-symlink("/firmware-modem/image/adsp.b04", "/system/etc/firmware/adsp.b04");
-symlink("/firmware-modem/image/adsp.b05", "/system/etc/firmware/adsp.b05");
-symlink("/firmware-modem/image/adsp.b06", "/system/etc/firmware/adsp.b06");
-symlink("/firmware-modem/image/adsp.b08", "/system/etc/firmware/adsp.b08");
-symlink("/firmware-modem/image/adsp.b09", "/system/etc/firmware/adsp.b09");
-symlink("/firmware-modem/image/adsp.b10", "/system/etc/firmware/adsp.b10");
-symlink("/firmware-modem/image/adsp.b11", "/system/etc/firmware/adsp.b11");
-symlink("/firmware-modem/image/adsp.b12", "/system/etc/firmware/adsp.b12");
-symlink("/firmware-modem/image/adsp.b13", "/system/etc/firmware/adsp.b13");
-symlink("/firmware-modem/image/adsp.mdt", "/system/etc/firmware/adsp.mdt");
-symlink("/firmware-modem/image/mba.b00", "/system/etc/firmware/mba.b00");
-symlink("/firmware-modem/image/mba.mdt", "/system/etc/firmware/mba.mdt");
-symlink("/firmware-modem/image/modem.b00", "/system/etc/firmware/modem.b00");
-symlink("/firmware-modem/image/modem.b01", "/system/etc/firmware/modem.b01");
-symlink("/firmware-modem/image/modem.b02", "/system/etc/firmware/modem.b02");
-symlink("/firmware-modem/image/modem.b03", "/system/etc/firmware/modem.b03");
-symlink("/firmware-modem/image/modem.b04", "/system/etc/firmware/modem.b04");
-symlink("/firmware-modem/image/modem.b05", "/system/etc/firmware/modem.b05");
-symlink("/firmware-modem/image/modem.b08", "/system/etc/firmware/modem.b08");
-symlink("/firmware-modem/image/modem.b10", "/system/etc/firmware/modem.b10");
-symlink("/firmware-modem/image/modem.b11", "/system/etc/firmware/modem.b11");
-symlink("/firmware-modem/image/modem.b13", "/system/etc/firmware/modem.b13");
-symlink("/firmware-modem/image/modem.b14", "/system/etc/firmware/modem.b14");
-symlink("/firmware-modem/image/modem.b15", "/system/etc/firmware/modem.b15");
-symlink("/firmware-modem/image/modem.b16", "/system/etc/firmware/modem.b16");
-symlink("/firmware-modem/image/modem.b17", "/system/etc/firmware/modem.b17");
-symlink("/firmware-modem/image/modem.b18", "/system/etc/firmware/modem.b18");
-symlink("/firmware-modem/image/modem.b19", "/system/etc/firmware/modem.b19");
-symlink("/firmware-modem/image/modem.b20", "/system/etc/firmware/modem.b20");
-symlink("/firmware-modem/image/modem.b21", "/system/etc/firmware/modem.b21");
-symlink("/firmware-modem/image/modem.b22", "/system/etc/firmware/modem.b22");
-symlink("/firmware-modem/image/modem.b25", "/system/etc/firmware/modem.b25");
-symlink("/firmware-modem/image/modem.b26", "/system/etc/firmware/modem.b26");
-symlink("/firmware-modem/image/modem.b27", "/system/etc/firmware/modem.b27");
-symlink("/firmware-modem/image/modem.mdt", "/system/etc/firmware/modem.mdt");
-symlink("/firmware/image/dtcpip.b00", "/system/etc/firmware/dtcpip.b00");
-symlink("/firmware/image/dtcpip.b01", "/system/etc/firmware/dtcpip.b01");
-symlink("/firmware/image/dtcpip.b02", "/system/etc/firmware/dtcpip.b02");
-symlink("/firmware/image/dtcpip.b03", "/system/etc/firmware/dtcpip.b03");
-symlink("/firmware/image/dtcpip.mdt", "/system/etc/firmware/dtcpip.mdt");
-symlink("/firmware/image/mc_v2.b00", "/system/etc/firmware/mc_v2.b00");
-symlink("/firmware/image/mc_v2.b01", "/system/etc/firmware/mc_v2.b01");
-symlink("/firmware/image/mc_v2.b02", "/system/etc/firmware/mc_v2.b02");
-symlink("/firmware/image/mc_v2.b03", "/system/etc/firmware/mc_v2.b03");
-symlink("/firmware/image/mc_v2.mdt", "/system/etc/firmware/mc_v2.mdt");
-symlink("/firmware/image/playread.b00", "/system/etc/firmware/playread.b00");
-symlink("/firmware/image/playread.b01", "/system/etc/firmware/playread.b01");
-symlink("/firmware/image/playread.b02", "/system/etc/firmware/playread.b02");
-symlink("/firmware/image/playread.b03", "/system/etc/firmware/playread.b03");
-symlink("/firmware/image/playread.mdt", "/system/etc/firmware/playread.mdt");
-symlink("/firmware/image/sec_stor.b00", "/system/etc/firmware/sec_stor.b00");
-symlink("/firmware/image/sec_stor.b01", "/system/etc/firmware/sec_stor.b01");
-symlink("/firmware/image/sec_stor.b02", "/system/etc/firmware/sec_stor.b02");
-symlink("/firmware/image/sec_stor.b03", "/system/etc/firmware/sec_stor.b03");
-symlink("/firmware/image/sec_stor.mdt", "/system/etc/firmware/sec_stor.mdt");
-symlink("/firmware/image/skm.b00", "/system/etc/firmware/skm.b00");
-symlink("/firmware/image/skm.b01", "/system/etc/firmware/skm.b01");
-symlink("/firmware/image/skm.b02", "/system/etc/firmware/skm.b02");
-symlink("/firmware/image/skm.b03", "/system/etc/firmware/skm.b03");
-symlink("/firmware/image/skm.mdt", "/system/etc/firmware/skm.mdt");
-symlink("/firmware/image/skmm_ta.b00", "/system/etc/firmware/skmm_ta.b00");
-symlink("/firmware/image/skmm_ta.b01", "/system/etc/firmware/skmm_ta.b01");
-symlink("/firmware/image/skmm_ta.b02", "/system/etc/firmware/skmm_ta.b02");
-symlink("/firmware/image/skmm_ta.b03", "/system/etc/firmware/skmm_ta.b03");
-symlink("/firmware/image/skmm_ta.mdt", "/system/etc/firmware/skmm_ta.mdt");
-symlink("/firmware/image/sshdcpap.b00", "/system/etc/firmware/sshdcpap.b00");
-symlink("/firmware/image/sshdcpap.b01", "/system/etc/firmware/sshdcpap.b01");
-symlink("/firmware/image/sshdcpap.b02", "/system/etc/firmware/sshdcpap.b02");
-symlink("/firmware/image/sshdcpap.b03", "/system/etc/firmware/sshdcpap.b03");
-symlink("/firmware/image/sshdcpap.mdt", "/system/etc/firmware/sshdcpap.mdt");
-symlink("/firmware/image/tima_atn.b00", "/system/etc/firmware/tima_atn.b00");
-symlink("/firmware/image/tima_atn.b01", "/system/etc/firmware/tima_atn.b01");
-symlink("/firmware/image/tima_atn.b02", "/system/etc/firmware/tima_atn.b02");
-symlink("/firmware/image/tima_atn.b03", "/system/etc/firmware/tima_atn.b03");
-symlink("/firmware/image/tima_atn.mdt", "/system/etc/firmware/tima_atn.mdt");
-symlink("/firmware/image/tima_key.b00", "/system/etc/firmware/tima_key.b00");
-symlink("/firmware/image/tima_key.b01", "/system/etc/firmware/tima_key.b01");
-symlink("/firmware/image/tima_key.b02", "/system/etc/firmware/tima_key.b02");
-symlink("/firmware/image/tima_key.b03", "/system/etc/firmware/tima_key.b03");
-symlink("/firmware/image/tima_key.mdt", "/system/etc/firmware/tima_key.mdt");
-symlink("/firmware/image/tima_lkm.b00", "/system/etc/firmware/tima_lkm.b00");
-symlink("/firmware/image/tima_lkm.b01", "/system/etc/firmware/tima_lkm.b01");
-symlink("/firmware/image/tima_lkm.b02", "/system/etc/firmware/tima_lkm.b02");
-symlink("/firmware/image/tima_lkm.b03", "/system/etc/firmware/tima_lkm.b03");
-symlink("/firmware/image/tima_lkm.mdt", "/system/etc/firmware/tima_lkm.mdt");
-symlink("/firmware/image/tima_pkm.b00", "/system/etc/firmware/tima_pkm.b00");
-symlink("/firmware/image/tima_pkm.b01", "/system/etc/firmware/tima_pkm.b01");
-symlink("/firmware/image/tima_pkm.b02", "/system/etc/firmware/tima_pkm.b02");
-symlink("/firmware/image/tima_pkm.b03", "/system/etc/firmware/tima_pkm.b03");
-symlink("/firmware/image/tima_pkm.mdt", "/system/etc/firmware/tima_pkm.mdt");
-symlink("/firmware/image/tzpr25.b00", "/system/etc/firmware/tzpr25.b00");
-symlink("/firmware/image/tzpr25.b01", "/system/etc/firmware/tzpr25.b01");
-symlink("/firmware/image/tzpr25.b02", "/system/etc/firmware/tzpr25.b02");
-symlink("/firmware/image/tzpr25.b03", "/system/etc/firmware/tzpr25.b03");
-symlink("/firmware/image/tzpr25.mdt", "/system/etc/firmware/tzpr25.mdt");
-symlink("/firmware/image/venus.b00", "/system/etc/firmware/venus.b00");
-symlink("/firmware/image/venus.b01", "/system/etc/firmware/venus.b01");
-symlink("/firmware/image/venus.b02", "/system/etc/firmware/venus.b02");
-symlink("/firmware/image/venus.b03", "/system/etc/firmware/venus.b03");
-symlink("/firmware/image/venus.b04", "/system/etc/firmware/venus.b04");
-symlink("/firmware/image/venus.mdt", "/system/etc/firmware/venus.mdt");
-symlink("/firmware/image/widevine.b00", "/system/vendor/firmware/widevine.b00");
-symlink("/firmware/image/widevine.b01", "/system/vendor/firmware/widevine.b01");
-symlink("/firmware/image/widevine.b02", "/system/vendor/firmware/widevine.b02");
-symlink("/firmware/image/widevine.b03", "/system/vendor/firmware/widevine.b03");
-symlink("/firmware/image/widevine.mdt", "/system/vendor/firmware/widevine.mdt");
-ui_print("Update metadata...");"""
-      return
 
 def RemoveCount(info):
   edify = info.script
@@ -211,6 +84,11 @@ def RemoveCount(info):
     if "ui_print" in edify.script[i] and "Update" and "Boot" and "image..." in edify.script[i]:
       edify.script[i] = 'delete("/data/system/count");'
       return
+
+def UpdatePerm(info):
+    extra_Perm = """set_metadata_recursive("/system/etc/init.d", "uid", 0, "gid", 2000, "dmode", 0755, "fmode", 0755, "capabilities", 0x0, "selabel", "u:object_r:system_file:s0");
+set_metadata("/system/etc/init.d", "uid", 0, "gid", 0, "mode", 0755, "capabilities", 0x0, "selabel", "u:object_r:system_file:s0");"""
+    info.script.AppendExtra(extra_Perm);
 
 def UpdateFirmWare(info):
     extra_firmware = """ui_print("Update FirmWare...");
@@ -343,4 +221,5 @@ def FullOTA_InstallEnd(info):
 	
 def IncrementalOTA_InstallEnd(info):
     RemoveDeviceAssert(info)
+    UpdatePerm(info):
     Writeboot(info)
