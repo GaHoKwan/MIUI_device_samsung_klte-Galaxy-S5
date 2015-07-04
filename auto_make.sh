c0c5e991c7a079d90fa64aa9567e242ac0d8db54
GaHoKwan <kwangaho@gmail.com>
2015-07-04 08:21:14 +0800

    update

--- auto_make.sh
+++ auto_make.sh
@@ -19,9 +19,8 @@ if [ "$1" == "" ];then
 	exit
 fi
 
-if [ -e out/ZIP/system/app/com.android.nfc-1.apk ];then
+if [ -e out/ZIP/system/priv-app/QuickSearchBox.apk ];then
 	cp -f stockrom/system/build.prop out/ZIP/system/build.prop
-	mv out/ZIP/system/app/com.android.nfc-1.apk out/ZIP/system/app/Nfc.apk
 	mv out/ZIP/system/priv-app/QuickSearchBox.apk out/ZIP/system/app/QuickSearchBox.apk
 fi
 
@@ -61,10 +60,8 @@ fullota(){
 	echo "============================"
 	echo ">>>  Copying Fullota.zip    "
 	echo "============================"
-	cp out/fullota.zip miui6_KLTE_luo2888_"$BUILD_NUMBER"_"$GET_FULLOTA_MD5SUM"_4.4.zip
+	cp out/fullota.zip miui6_KLTE_luo2888_"$BUILD_NUMBER"__4.4.zip
 }
-
-GET_DIFFOTA_MD5SUM=`md5sum $OUTDIR/diffota.zip |cut -c 1-10`
 
 otadiff(){
 	echo "============================"
@@ -76,7 +73,7 @@ otadiff(){
 	read m
 	if [ "$m" == "y" ]; then
 		../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $LAST_TARGET out/target_files.zip out/diffota.zip
-		cp out/diffota.zip miui_ota_KLTE_luo2888_"$LAST_BUILD_NUMBER"_"$BUILD_NUMBER"_"$GET_DIFFOTA_MD5SUM"_4.4.zip
+		cp out/diffota.zip miui_ota_KLTE_luo2888_"$LAST_BUILD_NUMBER"_"$BUILD_NUMBER"__4.4.zip
 		echo "================================="
 		echo ">>>  Copying  Target_files.zip   "
 		echo "================================="
@@ -86,11 +83,17 @@ otadiff(){
 	fi
 }
 
+OUTDIR=/home/jay/miui/v6-kitkat/klte/out
 
 init
 clean
 fullota
 otadiff
+
+if [ -e $OUTDIR/fullota.zip ]; then
+	source md5.sh
+fi
+
 echo "================================="
 echo "             Done!               "
 echo "================================="
